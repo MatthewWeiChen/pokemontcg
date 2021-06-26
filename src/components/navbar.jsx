a9f61729920f8b07e7b7f6c43a6a58a610ae4c83
MatthewWeiChen <mattchen1021@gmail.com>
2021-06-25 21:22:13 -0700

    added pokeball img and dark features

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import pokeball from '../images/Poke_Ball.png';
 
 class NavbarPokemon extends React.Component {
   render() {
     return (
       <>
-        <Navbar bg="dark" expand="xl">
+        <Navbar bg="dark" expand="xl" variant="dark">
           <Navbar.Brand href="#home" className="text-white">
-            <img
-              src="../images/Poke_Ball.png"
-              width="30"
-              height="30"
-              alt="Pokeball"
-            />
-            Pokemon TCG
+            <img src={pokeball} width="30" height="30" alt="pokeball" /> Pokemon
+            TCG
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Collapse id="basic-navbar-nav" className="white">
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#link">Link</Nav.Link>
